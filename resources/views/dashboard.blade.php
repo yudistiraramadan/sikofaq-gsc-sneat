174aa36c9ad4bc5bf04fb0f4dc3958c483cb56ff
yudistira-rk <yudistiraeducation100@gmail.com>
2023-03-16 08:50:39 +0700

    16/03/2023 mulai hari dengan coding

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -6,4 +6,7 @@
     @if (auth()->user()->role_id=='2')
     <h5>Selamat Datang Petugas</h5>
     @endif
+    @if (auth()->user()->role_id=='3')
+    <h5>Selamat Datang Bendahara</h5>
+    @endif
 @endsection
--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -112,12 +112,12 @@
       </li>
 
       <li class="menu-header small text-uppercase">
-        <span class="menu-header-text">Pages</span>
+        <span class="menu-header-text">User</span>
       </li>
       <li class="menu-item">
         <a href="javascript:void(0);" class="menu-link menu-toggle">
           <i class="menu-icon tf-icons bx bx-dock-top"></i>
-          <div data-i18n="Account Settings">Account Settings</div>
+          <div data-i18n="Account Settings">Daftar User</div>
         </a>
         <ul class="menu-sub">
           <li class="menu-item">
