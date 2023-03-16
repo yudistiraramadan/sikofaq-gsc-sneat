f18a8e776cb19dae0abfea2d9578dea621aeac16
yudistira-rk <yudistiraeducation100@gmail.com>
2023-03-16 13:47:27 +0700

    16/03/2023 daftar user relasi 3 tablel

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,11 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
     public function user(){
-        return view('user.daftar-user');
+        $user = User::join('detail_users', 'users.id', '=', 'detail_users.user_id', 'users')
+        ->join('roles', 'users.role_id', '=', 'roles.id')
+        ->get(['users.name', 'detail_users.address', 'detail_users.phone', 'roles.role_name']);
+
+
+        return view('user.daftar-user', compact('user'));
     }
 }
--- resources/views/user/daftar-user.blade.php
+++ resources/views/user/daftar-user.blade.php
@@ -17,7 +17,7 @@
         </tr>
       </thead>
       <tbody class="table-border-bottom-0">
-        <tr>
+        {{-- <tr>
           <td><i class="fab fa-angular fa-lg text-danger me-3"></i> <strong>Yudis Ganzz</strong></td>
           <td>Jln. Sulawesi Perum Puri Tanjung Intan No.B2 Gunung Simping</td>
           <td>085228409840</td>
@@ -31,7 +31,25 @@
               </div>
             </div>
           </td>
+        </tr> --}}
+        @foreach ($user as $data)
+        <tr>
+          <td><i class="fab fa-angular fa-lg text-danger me-3"></i>{{ $data->name }}</td>
+          <td><i class="fab fa-angular fa-lg text-danger me-3"></i>{{ $data->address }}</td>
+          <td><i class="fab fa-angular fa-lg text-danger me-3"></i>{{ $data->phone }}</td>
+          <td><i class="fab fa-angular fa-lg text-danger me-3"></i>{{ $data->role_name }}</td>
+          <td>
+            <div class="dropdown">
+              <button type="button" class="btn p-0 dropdown-toggle hide-arrow" data-bs-toggle="dropdown"><i class="bx bx-dots-vertical-rounded"></i></button>
+              <div class="dropdown-menu">
+                <a class="dropdown-item" href="javascript:void(0);"><i class="bx bx-edit-alt me-1"></i> Edit</a>
+                <a class="dropdown-item" href="javascript:void(0);"><i class="bx bx-trash me-1"></i> Delete</a>
+              </div>
+            </div>
+          </td>
+
         </tr>
+        @endforeach
       </tbody>
     </table>
   </div>
