f38267f5c4f35686317d07a27cc2d586f0b6ab3e
yudistira-rk <yudistiraeducation100@gmail.com>
2023-03-17 10:24:37 +0700

    17/03/2023 datatables

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,24 +1,26 @@
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport"content="width=device-width, initial-scale=1.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0">
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0" />
 
-    <title>{{ $title ?? 'SIKOFAQ-GSC' }}</title>
+    <title>Account settings - Pages | Sneat - Bootstrap 5 HTML Admin Template - Pro</title>
 
-    {{-- <meta name="description" content=""> --}}
+    <meta name="description" content="Most Powerful &amp; Comprehensive Bootstrap 5 HTML Admin Dashboard Template built for developers!" />
+    <meta name="keywords" content="dashboard, bootstrap 5 dashboard, bootstrap 5 design, bootstrap 5">
+    <!-- Canonical SEO -->
+    <link rel="canonical" href="https://themeselection.com/products/sneat-bootstrap-html-admin-template/">
 
     <!-- Favicon -->
     <link rel="icon" type="image/x-icon" href="sneat/assets/img/favicon/favicon.ico" />
 
     <!-- Fonts -->
-    <link rel="preconnect" href="https://fonts.googleapis.com" />
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-    <link
-      href="https://fonts.googleapis.com/css2?family=Public+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap"
-      rel="stylesheet"
-    />
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Public+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap" rel="stylesheet">
 
     <!-- Icons. Uncomment required icon fonts -->
     <link rel="stylesheet" href="sneat/assets/vendor/fonts/boxicons.css" />
+
+
 
     <!-- Core CSS -->
     <link rel="stylesheet" href="sneat/assets/vendor/css/core.css" class="template-customizer-core-css" />
@@ -28,7 +30,7 @@
     <!-- Vendors CSS -->
     <link rel="stylesheet" href="sneat/assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.css" />
 
-    <link rel="stylesheet" href="sneat/assets/vendor/libs/apex-charts/apex-charts.css" />
+
 
     <!-- Page CSS -->
 
@@ -39,6 +41,23 @@
     <!--? Config:  Mandatory theme config file contain global vars & default theme options, Set your preferred theme option in this file.  -->
     <script src="sneat/assets/js/config.js"></script>
 
-    <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.4/dist/jquery.min.js"></script>
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async="async" src="https://www.googletagmanager.com/gtag/js?id=GA_MEASUREMENT_ID"></script>
+    <script>
+    window.dataLayer = window.dataLayer || [];
 
-  </head>
+    function gtag() {
+      dataLayer.push(arguments);
+    }
+    gtag('js', new Date());
+    gtag('config', 'GA_MEASUREMENT_ID');
+    </script>
+    <!-- Custom notification for demo -->
+    <!-- beautify ignore:end -->
+
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.4/dist/jquery.min.js"></script>
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.13.4/css/jquery.dataTables.min.css">
+    <script src="https://cdn.datatables.net/1.13.4/js/jquery.dataTables.min.js"></script>
+
+
+</head>
--- resources/views/user/daftar-user.blade.php
+++ resources/views/user/daftar-user.blade.php
@@ -6,7 +6,7 @@
 <p><div class="badge bg-primary">3</div>&nbsp; Total User</p>
 
 <div class="table-responsive text-nowrap ">
-    {{-- <table class="table table-bordered table-hover" id="dt-user">
+    <table class="table table-bordered table-hover" id="dt-user">
       <thead>
         <tr>
           <th>Nama</th>
@@ -16,7 +16,7 @@
           <th>Aksi</th>
         </tr>
       </thead>
-      <tbody class="table-border-bottom-0"> --}}
+      <tbody class="table-border-bottom-0">
         {{-- <tr>
           <td><i class="fab fa-angular fa-lg text-danger me-3"></i> <strong>Yudis Ganzz</strong></td>
           <td>Jln. Sulawesi Perum Puri Tanjung Intan No.B2 Gunung Simping</td>
@@ -32,7 +32,7 @@
             </div>
           </td>
         </tr> --}}
-        {{-- @foreach ($user as $data)
+        @foreach ($user as $data)
         <tr>
           <td><i class="fab fa-angular fa-lg text-danger me-3"></i>{{ $data->name }}</td>
           <td><i class="fab fa-angular fa-lg text-danger me-3"></i>{{ $data->address }}</td>
@@ -51,44 +51,13 @@
         </tr>
         @endforeach
       </tbody>
-    </table> --}}
-    <table id="dt-tes" class="display" style="width:100%">
-        <thead>
-            <tr>
-                <th>Name</th>
-                <th>Position</th>
-                <th>Office</th>
-                <th>Age</th>
-                <th>Start date</th>
-                <th>Salary</th>
-            </tr>
-        </thead>
-        <tbody>
-            <tr>
-                <td>Tiger Nixon</td>
-                <td>System Architect</td>
-                <td>Edinburgh</td>
-                <td>61</td>
-                <td>2011-04-25</td>
-                <td>$320,800</td>
-            </tr>
-            <tr>
-                <td>Garrett Winters</td>
-                <td>Accountant</td>
-                <td>Tokyo</td>
-                <td>63</td>
-                <td>2011-07-25</td>
-                <td>$170,750</td>
-            </tr>
-        </tbody>
     </table>
 
+
 </div>
-@endsection
-@push('scripts')
+{{-- @push('scripts') --}}
 <script>
-    $(document).ready(function() {
-        $('#dt-tes').DataTable();
-    });
+    let table = new DataTable('#dt-user');
 </script>
-@endpush
+{{-- @endpush --}}
+@endsection
